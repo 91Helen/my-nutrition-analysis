0d07fafa44224315d9536ecbe2d40e8d6b9b0b0e
91Helen <157799017+91Helen@users.noreply.github.com>
2025-06-13 16:34:46 +0500

    Update Nutrition.js

--- src/Nutrition.js
+++ src/Nutrition.js
@@ -1,7 +1,29 @@
-export const Nutrition = ({ label, quantity, unit }) => {
-    return (
-        <div>
-            <p><b>{label}</b> - {quantity} {unit}</p>
+function Nutrition () {
+const NutritionShow = ({label,quantity,unit}) => {
+
+
+    return(
+        
+  <div className="App">
+            <table>
+                <tr>
+                    <th>label</th>
+                    <th>quantity</th>
+                    <th>unit</th>
+                </tr>
+                {NutritionShow.map((val, key) => {
+                    return (
+                        <tr key={key}>
+                            <td>{val.label}</td>
+                            <td>{val.quantity}</td>
+                            <td>{val.unit}</td>
+                        </tr>
+                    )
+                })}
+            </table>
         </div>
+        
     )
 }
+}
+export default Nutrition;
